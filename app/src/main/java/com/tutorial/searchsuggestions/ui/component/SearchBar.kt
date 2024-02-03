3471d22d4569a881abd2e825cf6d12af939ec98b
Benedict Lim <lim.benedict@moneyforward.co.jp>
2024-02-03 22:31:43 +0900

    refactor: use TextField instead to create SearchBar

--- app/src/main/java/com/tutorial/searchsuggestions/ui/component/SearchBar.kt
+++ app/src/main/java/com/tutorial/searchsuggestions/ui/component/SearchBar.kt
@@ -1,12 +1,19 @@
 package com.tutorial.searchsuggestions.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,33 +22,40 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tutorial.searchsuggestions.R
 import com.tutorial.searchsuggestions.ui.theme.AppTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
+    modifier: Modifier = Modifier,
     query: String,
     onQueryChange: (String) -> Unit,
-    onSearch: (String) -> Unit,
+    onSearch: KeyboardActionScope.() -> Unit,
     onClose: () -> Unit,
-    onClear: () -> Unit,
-    content: @Composable ColumnScope.() -> Unit
+    onClear: () -> Unit
 ) {
-    var active by remember { mutableStateOf(false) }
-
-    androidx.compose.material3.SearchBar(
-        query = query,
-        onQueryChange = onQueryChange,
-        onSearch = onSearch,
-        active = active,
-        onActiveChange = { active = it },
+    OutlinedTextField(
+        modifier = modifier,
+        value = query,
+        onValueChange = { onQueryChange(it) },
+        placeholder = { PlaceholderText() },
         leadingIcon = { CloseButton(onClick = onClose) },
-        trailingIcon = { ClearButton(onClick = onClear) }
-    ) {
-        content()
-    }
+        trailingIcon = { ClearButton(onClick = onClear) },
+        keyboardActions = KeyboardActions(onSearch = onSearch),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
+    )
+}
+
+@Composable
+private fun PlaceholderText() {
+    Text(
+        text = stringResource(R.string.placeholder_search_bar),
+        color = Color.LightGray,
+        style = MaterialTheme.typography.bodyMedium
+    )
 }
 
 @Composable
@@ -70,14 +84,18 @@ private fun PreviewSearchBar() {
     var query by remember { mutableStateOf("") }
 
     AppTheme {
-        SearchBar(
-            query = query,
-            onQueryChange = { query = it },
-            onSearch = {},
-            onClose = {},
-            onClear = {}
+        Box(
+            modifier = Modifier
+                .background(color = Color.White)
+                .padding(16.dp)
         ) {
-            // TODO: Add content
+            SearchBar(
+                query = query,
+                onQueryChange = { query = it },
+                onSearch = {},
+                onClose = {},
+                onClear = { query = "" }
+            )
         }
     }
 }
