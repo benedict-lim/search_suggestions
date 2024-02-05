2ce0b08deea276be43638566dab8976b2cb686ad
Benedict Lim <lim.benedict@moneyforward.co.jp>
2024-02-05 23:32:39 +0900

    feat: create search flow

--- app/src/main/java/com/tutorial/searchsuggestions/ui/scene/SearchScene.kt
+++ app/src/main/java/com/tutorial/searchsuggestions/ui/scene/SearchScene.kt
@@ -1,7 +1,85 @@
 package com.tutorial.searchsuggestions.ui.scene
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import com.tutorial.searchsuggestions.ui.component.SearchBar
+import com.tutorial.searchsuggestions.ui.component.SearchSuggestionsList
+import com.tutorial.searchsuggestions.viewmodel.SearchViewModel
+import com.tutorial.searchsuggestions.viewmodel.uistate.SearchUiState
 
 @Composable
-fun SearchScene() {
+fun SearchScene(viewModel: SearchViewModel = hiltViewModel()) {
+    val navController = rememberNavController()
+
+    val uiState = viewModel.uiState.collectAsState().value
+
+    Scaffold(
+        topBar = {
+            SearchBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                query = uiState.query,
+                onQueryChange = { viewModel.onQueryChange(it) },
+                onSearch = { /*TODO*/ },
+                onClose = { navController.popBackStack() },
+                onClear = { viewModel.onClear() }
+            )
+        },
+        content = { padding ->
+            SearchSceneContent(
+                contentPadding = padding,
+                uiState = uiState,
+                onSelect = { viewModel.onQueryChange(it) }
+            )
+        }
+    )
+}
+
+@Composable
+private fun SearchSceneContent(
+    contentPadding: PaddingValues,
+    uiState: SearchUiState,
+    onSelect: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(contentPadding),
+    ) {
+        // Show linear progress indicator when loading, otherwise a divider in place
+        if (uiState.isLoading) {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+        } else {
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.DarkGray
+            )
+        }
+
+        uiState.response?.let {
+            SearchSuggestionsList(
+                keyword = it.keyword,
+                results = it.results,
+                onSelect = onSelect
+            )
+        }
+
+        uiState.error?.let {
+            /*TODO*/
+        }
+    }
 }
--- app/src/main/java/com/tutorial/searchsuggestions/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/tutorial/searchsuggestions/viewmodel/SearchViewModel.kt
@@ -1,12 +1,13 @@
 package com.tutorial.searchsuggestions.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tutorial.searchsuggestions.api.model.SearchSuggestionsResponse
 import com.tutorial.searchsuggestions.usecase.GetSearchSuggestionsUseCase
 import com.tutorial.searchsuggestions.viewmodel.uistate.SearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,16 +17,20 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle,
     private val useCase: GetSearchSuggestionsUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(SearchUiState())
     val uiState: StateFlow<SearchUiState> = _uiState.asStateFlow()
 
-    fun search() {
+    private var job: Job? = null
+
+    private fun search() {
         if (uiState.value.query.isBlank() || uiState.value.isLoading) return
 
-        viewModelScope.launch {
+        job = viewModelScope.launch {
+            // Delay to ensure no more input before executing search
+            delay(DEBOUNCE_TIME_MS)
+
             _uiState.update { it.copy(isLoading = true) }
 
             runCatching { useCase.getSearchSuggestions(uiState.value.query) }
@@ -44,9 +49,18 @@ class SearchViewModel @Inject constructor(
 
     fun onQueryChange(value: String) {
         _uiState.update { it.copy(query = value) }
+
+        // Cancel job if underway to re-execute search based on new query
+        job?.cancel()
+
+        search()
     }
 
     fun onClear() {
         _uiState.update { it.copy(query = "") }
+    }
+
+    private companion object {
+        const val DEBOUNCE_TIME_MS = 300L
     }
 }
